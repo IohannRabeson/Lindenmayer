516195481c8bebe4d891ccb0fb6f2634528d8286
irabeson <IohannRabeson@users.noreply.github.com>
2018-04-19 22:55:26 -0400

    Boost::optional now imported as sub-repository.

--- ci/travis/linux/build.sh
+++ ci/travis/linux/build.sh
@@ -2,9 +2,8 @@ set -e
 
 # install more recent Qt 5.
 sudo apt-add-repository --yes ppa:beineri/opt-qt-5.10.1-trusty
-sudo apt-add-repository --yes ppa:nschloe/boost-nightly
 sudo apt-get -qq update
-sudo apt-get --yes install qt510-meta-minimal qt510tools qt510location boost1.66
+sudo apt-get --yes install qt510-meta-minimal qt510tools qt510location
 sudo /opt/qt510/bin/qt510-env.sh
 
 mkdir builds
