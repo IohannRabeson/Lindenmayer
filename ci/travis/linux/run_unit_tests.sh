a1c53204ed535231f309505ef8ba5ccd1bb42468
irabeson <IohannRabeson@users.noreply.github.com>
2019-03-17 19:20:23 -0400

    Fix path and add functional tests on linux.

--- ci/travis/linux/run_unit_tests.sh
+++ ci/travis/linux/run_unit_tests.sh
@@ -1,1 +1,5 @@
-./builds/tests/unit-tests
+
+# Run unit tests
+./builds/tests/unit-tests/unit-tests
+# Run simple functional tests
+./builds/app/Lindenmayer --platform offscreen --auto-test ./tests/lcode-examples
--- ci/travis/osx/run_unit_tests.sh
+++ ci/travis/osx/run_unit_tests.sh
@@ -1,6 +1,6 @@
 
 # Run unit tests
-./builds/tests/unit_tests
+./builds/tests/unit_tests/unit_tests
 
 # Run simple functional tests
 ./builds/app/Lindenmayer.app/Contents/MacOS/Lindenmayer --auto-test ./tests/lcode-examples
