f26ff0f2bb402366e2419302cef048c4b2645d27
irabeson <IohannRabeson@users.noreply.github.com>
2019-03-20 23:59:48 -0400

    Add comments and documentation.
    [ci skip]

--- ci/travis/osx/run_unit_tests.sh
+++ ci/travis/osx/run_unit_tests.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+# Address sanitizer options
 ASAN_OPTIONS=symbolize=1
 ASAN_SYMBOLIZER_PATH="$(brew --prefix llvm)/bin/llvm-symbolizer"
+ASAN_OPTIONS=check_initialization_order=1
+
+# Thread sanitizer options
 TSAN_OPTIONS=symbolize=1
 TSAN_SYMBOLIZER_PATH="$(brew --prefix llvm)/bin/llvm-symbolizer"
+
+# Undefined behavior sanitizer options
 USAN_OPTIONS=symbolize=1
 USAN_SYMBOLIZER_PATH="$(brew --prefix llvm)/bin/llvm-symbolizer"
 
